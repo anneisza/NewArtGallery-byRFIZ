4be12b2b7e50dd6c4a96b3facd41e6a310f5fc41
anneisza <rizaanneisza@gmail.com>
2026-06-03 06:31:40 +0700

    pass rev

--- src/main/java/com/galeriseni/galeri_seni/controller/AuthController.java
+++ src/main/java/com/galeriseni/galeri_seni/controller/AuthController.java
@@ -1,8 +1,11 @@
 // src/main/java/com/galeriseni/galeri_seni/controller/AuthController.java
 package com.galeriseni.galeri_seni.controller;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AuthController {
@@ -11,7 +14,15 @@ public class AuthController {
     public String loginPage() {
         return "auth/login";
     }
+
+    @GetMapping("/hash")
+    @ResponseBody
+    public String hash(@RequestParam String pw) {
+        return new BCryptPasswordEncoder(10).encode(pw);
+    }
 }
+
+
 
 
 
